c761241439e1853a93e3c561ba61e82d8ac2fcce
Yuuki-Ishino <yyuuuukkii0914@gmail.com>
2025-09-12 03:31:52 +0900

    fix: redirectTo url

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: 'http://localhost:3000/auth/callback', // OAuth完了後に戻るページ
+        redirectTo: '/auth/callback', // OAuth完了後に戻るページ
       },
     });
   };
