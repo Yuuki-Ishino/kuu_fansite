a8a450cc530725670df65047d1cb86ece034a97b
Yuuki-Ishino <yyuuuukkii0914@gmail.com>
2025-09-25 11:39:02 +0900

    add: openGraph metadata in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,18 +3,29 @@ import Footer from "./top-components/Footer";
 import "../styles/globals.css";
 
 export const metadata = {
-  metadataBase: "https://bbooth.vercel.app",
-  siteName: "Bbooth",
-  locale: "ja_JP",
-  type: "website",
+  title: "Bbooth",
+  description: "東洋大学公式ボランティアサークル Bbooth",
+  metadataBase: new URL("https://bbooth.vercel.app"),
   keywords: [
     "Bbooth",
     "東洋大学",
     "サークル",
     "ボランティアサークル",
     "学生ボランティア",
-  ], // サイト共通キーワード
+  ],
+  icons: {
+    icon: "/images/Bbooth_logo.ico",
+  },
+  openGraph: {
+    title: "Bbooth",
+    description: "東洋大学公式ボランティアサークル Bbooth",
+    url: "https://bbooth.vercel.app",
+    siteName: "Bbooth",
+    locale: "ja_JP",
+    type: "website",
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
